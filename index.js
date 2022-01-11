64dc20e195203e9f1644f21370ec46379963bb0f
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-01-11 11:45:48 -0800

    Small fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-let currentUser = "Enkhbat";
-let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const currentUser = "Grace Hopper";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
 let shortGreeting = "Welcome, " + currentUser.slice(0, 1) + "!";
